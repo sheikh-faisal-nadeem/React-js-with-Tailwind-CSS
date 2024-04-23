392b726a9dd12ea90badf38dd3b45025ee0cf798
Sheikh Faisal Nadeem <nfaisal679@gmail.com>
2024-04-24 04:20:53 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2  className='text-red-400 bg-black' >    
+
+          Tailwind CSS in React js !!!
+      </h2>
+    </div>
+  )
+}
+
+export default App
